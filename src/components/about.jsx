97d9def46b3f205f46bf5d81900906464e9083d5
Daniele Trovato <plasticsoup@yahoo.co.uk>
2022-10-16 22:08:25 +0100

    Add position-static to the navbar. Add style to about and expand on the text. Add style to the nav buttons

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,12 +2,15 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>How does it work</h1>
+    <div className="m-5 bg-info p-5 text-white">
+      <h1>How it works</h1>
       <p>
         This is an application to help flatmates work out what a fair share of
-        the rent is. It asks a user to calculate which percentage of the total
-        rent the common areas are. On that, every flatmate pays an equal share
+        the rent is. It asks a user to enter the price of the whole property,
+        then an estimate of what they think the value of the common areas is as
+        a percentage of the total rent. Below that it asks for the dimentions of
+        each room and how many people are gonna share that room. Every flatmate
+        pays an equal share on the percentage allocated to the common areas
         while on the remaining rent they pay proportionately according to the
         size of their room
       </p>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,32 +3,19 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="content">
-      <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-          <Link class="navbar-brand" to="/about" id="about">
-            About
+    <div className="content position-static">
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid justify-content-center">
+          <Link className="navbar-brand" to="/about" id="about">
+            <button className="btn btn-info text-white">About</button>
           </Link>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <Link class="nav-link active" aria-current="page" to="/">
-                  Home
-                </Link>
-              </li>
-            </ul>
-          </div>
+          <Link className="navbar-brand" aria-current="page" to="/">
+            <button className="btn btn-info text-white">Home</button>
+          </Link>
+
+          {/* <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+          </div> */}
         </div>
       </nav>
     </div>
