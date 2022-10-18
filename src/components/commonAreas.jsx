fc703e6eb593a156f3392f3fa64ee43c52a3a247
Daniele Trovato <plasticsoup@yahoo.co.uk>
2022-10-18 10:19:26 +0100

    Comment out line font awesome imports on common areas

--- src/components/commonAreas.jsx
+++ src/components/commonAreas.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faQuestion } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faQuestion } from "@fortawesome/free-solid-svg-icons";
 
 class CommonAreas extends Component {
   render() {
